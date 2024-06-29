61b9a29aa272dc26620f1729c08faa6d717a4355
tct123 <42028373+tct123@users.noreply.github.com>
2024-06-29 09:23:07 +0200

    .

--- pydroidappqt.py
+++ pydroidappqt.py
@@ -1,13 +1,17 @@
 import locale
-import mylocale
+from mylocale.TR import tr
 import sys
 from qtpy import QtWidgets, QtCore
 
 app = QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QMainWindow()
-label = QtWidgets.QLabel("Hello, world!\n\nThis example can also run with other Qt libraries like PyQt6, PyQt5 and PySide2 using QtPy library.")
+label = QtWidgets.QLabel("")
 label.setAlignment(QtCore.Qt.AlignCenter)
 label.setWordWrap(True)
 window.setCentralWidget(label)
+lang_label = QtWidgets.QLabel("")
+lang_label.setAlignment(QtCore.Qt.AlignCenter)
+lang_label.setWordWrap(True)
+window.setCentralWidget(lang_label)
 window.show()
 sys.exit(app.exec())
