36a63c9180b7b23f62089372ab1603d0dfba1360
tct123 <42028373+tct123@users.noreply.github.com>
2024-06-29 09:30:04 +0200

    Update pydroidappqt.py

--- pydroidappqt.py
+++ pydroidappqt.py
@@ -3,15 +3,18 @@ from mylocale.TR import tr
 import sys
 from qtpy import QtWidgets, QtCore
 
+lang = locale.getlocale()[0]
+print(lang)
+file = "localisation.csv"
 app = QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QMainWindow()
-label = QtWidgets.QLabel("")
+label = QtWidgets.QLabel(tr(csv_file=file, target_key="HELLO", langcode=lang))
 label.setAlignment(QtCore.Qt.AlignCenter)
 label.setWordWrap(True)
 window.setCentralWidget(label)
-lang_label = QtWidgets.QLabel("")
-lang_label.setAlignment(QtCore.Qt.AlignCenter)
-lang_label.setWordWrap(True)
-window.setCentralWidget(lang_label)
+#lang_label = QtWidgets.QLabel("")
+#lang_label.setAlignment(QtCore.Qt.AlignCenter)
+#lang_label.setWordWrap(True)
+#window.setCentralWidget(lang_label)
 window.show()
 sys.exit(app.exec())
